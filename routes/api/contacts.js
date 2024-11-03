338391f6bad8877880c3af2450e4d69073bf9703
Spudowski <156944179+Spudowski@users.noreply.github.com>
2024-11-03 14:10:11 +0100

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,50 @@
-const express = require('express')
+const express = require('express');
+const { listContacts, getById, addContact, removeContact, updateContact } = require('../../contactsController');
+const router = express.Router();
 
-const router = express.Router()
+router.get('/', async (req, res) => {
+  const contacts = await listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'hello world' })
-})
+router.get('/:id', async (req, res) => {
+  const contact = await getById(req.params.id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    return res.status(400).json({ message: "missing required name - field" });
+  }
+  const newContact = await addContact({ name, email, phone });
+  res.status(201).json(newContact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', async (req, res) => {
+  const deleted = await removeContact(req.params.id);
+  if (deleted) {
+    res.status(200).json({ message: "contact deleted" });
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:id', async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name && !email && !phone) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+  const updatedContact = await updateContact(req.params.id, req.body);
+  if (updatedContact) {
+    res.status(200).json(updatedContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
